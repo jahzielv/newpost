a1b20b0387f4ae8761acf2a164693966111b4059
Jahziel Villasana <jahzielv@gmail.com>
2019-04-18 09:14:46 -0400

    adding to travis

--- test/test.js
+++ test/test.js
@@ -1,14 +1,6 @@
 const assert = require("assert");
 const { addFrontMatter, createPost, clean } = require("../util");
 
-// describe("Array", () => {
-//     describe("#indexOf", () => {
-//         it("should return -1 when the value is not present", () => {
-//             assert.equal([1, 2, 3].indexOf(4), -1);
-//         });
-//     });
-// });
-
 describe("addFrontMatter", () => {
     it("should edit package.json correctly", async () => {
         let fmArr = ["a:b", "c:d", "e:f"];
