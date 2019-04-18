94b5998cda9dc3cd888e40538e1862954db4da93
Jahziel Villasana <jahzielv@gmail.com>
2019-04-18 09:07:02 -0400

    added test for addFrontMatter!

--- test/test.js
+++ test/test.js
@@ -1,9 +1,26 @@
 const assert = require("assert");
+const { addFrontMatter, createPost, clean } = require("../util");
 
-describe("Array", () => {
-    describe("#indexOf", () => {
-        it("should return -1 when the value is not present", () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+// describe("Array", () => {
+//     describe("#indexOf", () => {
+//         it("should return -1 when the value is not present", () => {
+//             assert.equal([1, 2, 3].indexOf(4), -1);
+//         });
+//     });
+// });
+
+describe("addFrontMatter", () => {
+    it("should edit package.json correctly", async () => {
+        let fmArr = ["a:b", "c:d", "e:f"];
+        let editProm = await addFrontMatter(fmArr);
+        let editedPJ = require("../package.json");
+        assert.deepStrictEqual(editedPJ.newpost, {
+            frontMatter: {
+                a: "b",
+                c: "d",
+                e: "f",
+                title: ""
+            }
         });
     });
 });
